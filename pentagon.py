eb3c838d21a7fa321dadb8af1b376768216bfb71
guillermo <porregu@ssfs.org>
2019-09-12 09:19:33 -0400

    i make a pentagon with 72 degrees angels

---
+++ pentagon.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.exiticket
